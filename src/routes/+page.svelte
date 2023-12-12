9a9f0292177a2588a6ad2fa142d97be9c77c4cea
Luc4s <111712686+LucasCaez@users.noreply.github.com>
2023-12-12 10:59:11 +0000

    jkh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+const options = {
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        'X-RapidAPI-Key': 'f19483e9fdmsha8ded5b27ca7f27p19b96fjsn70aa83597df6'
+    }
+}
+< /script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a> https://www.youtube.com/watch?v=4dkWsytM1kg min23
+https://rapidapi.com/weatherapi/api/weatherapi-com</a>
